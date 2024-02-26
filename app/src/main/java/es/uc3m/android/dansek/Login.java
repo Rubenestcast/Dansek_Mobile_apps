32dc31c081bd35ed4c3b6ec9a8cbdcfd2e6106e7
Rubenestcast <100475878@alumnos.uc3m.es>
2024-02-26 19:07:46 +0100

    Commit

--- app/src/main/java/es/uc3m/android/dansek/Login.java
+++ app/src/main/java/es/uc3m/android/dansek/Login.java
@@ -2,15 +2,45 @@ package es.uc3m.android.dansek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    private GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
 
+        FrameLayout frameLayout = findViewById(R.id.frameLayout);
+        TextView smallRectangle = findViewById(R.id.smallRectangle);
 
+        gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                float distanceX = e2.getX() - e1.getX();
+                if (distanceX > 0) {
+                    // Deslizamiento hacia la derecha
+                    Intent intent = new Intent(Login.this, SignUp.class);
+                    startActivity(intent);
+                }
+                return true;
+            }
+        });
+
+
+        smallRectangle.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return gestureDetector.onTouchEvent(event);
+            }
+        });
     }
 }
---
+++ app/src/main/java/es/uc3m/android/dansek/SignUp.java
@@ -0,0 +1,14 @@
+package es.uc3m.android.dansek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
