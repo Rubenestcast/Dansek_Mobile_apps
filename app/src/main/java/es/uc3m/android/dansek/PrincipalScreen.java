805d2e45f2572f79c5434de3d4bd6a2c6f3c6739
Rubenestcast <100475878@alumnos.uc3m.es>
2024-02-27 18:48:29 +0100

    Commit

---
+++ app/src/main/java/es/uc3m/android/dansek/PrincipalScreen.java
@@ -0,0 +1,14 @@
+package es.uc3m.android.dansek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PrincipalScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.principal_screen_layout);
+    }
+}
