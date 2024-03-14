33ebd3f9c024e50fd029a3aa7f759fca89692195
escuudero <100472781@alumnos.uc3m.es>
2024-03-14 18:41:34 +0100

    commit

---
+++ app/src/main/java/es/uc3m/android/dansek/tickets.java
@@ -0,0 +1,77 @@
+package es.uc3m.android.dansek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+public class Ticket {
+    private String id;
+    private String nombre;
+    private String empresa;
+    private String local;
+    private String dia;
+    private int numeroCopas;
+
+    public Ticket(String id, String nombre, String empresa, String local, String dia, int numeroCopas) {
+        this.id = id;
+        this.nombre = nombre;
+        this.empresa = empresa;
+        this.local = local;
+        this.dia = dia;
+        this.numeroCopas = numeroCopas;
+    }
+
+    // Métodos getter y setter para acceder a los atributos de la clase
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
+    public void setLocal(String local) {
+        this.local = local;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public int getNumeroCopas() {
+        return numeroCopas;
+    }
+
+    public void setNumeroCopas(int numeroCopas) {
+        this.numeroCopas = numeroCopas;
+    }
+}
