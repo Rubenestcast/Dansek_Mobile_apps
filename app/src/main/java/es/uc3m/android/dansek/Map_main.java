760894e6a38e4eaeaed3270c9542f683f9f208eb
Rubenestcast <100475878@alumnos.uc3m.es>
2024-03-21 12:24:13 +0100

    Commit

--- app/src/main/java/es/uc3m/android/dansek/Map_main.java
+++ app/src/main/java/es/uc3m/android/dansek/Map_main.java
@@ -2,9 +2,11 @@ package es.uc3m.android.dansek;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -17,9 +19,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class Map_main extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, GoogleMap.OnMapLongClickListener {
 
-    EditText txtLatitud, txtLongitud;
+    EditText txtLatitudLongitud, txtCalle;
     GoogleMap mMap;
 
     @Override
@@ -28,8 +34,8 @@ public class Map_main extends AppCompatActivity implements OnMapReadyCallback, G
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_main);
 
-        txtLatitud = findViewById(R.id.txtlatitud);
-        txtLongitud = findViewById(R.id.txtlongitud);
+        txtLatitudLongitud = findViewById(R.id.txtlatitudlongitud);
+        txtCalle = findViewById(R.id.txtcalle);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
@@ -54,23 +60,41 @@ public class Map_main extends AppCompatActivity implements OnMapReadyCallback, G
         this.mMap.setOnMapLongClickListener(this);
 
         LatLng madrid = new LatLng(40.408666,-3.6911323);
+
         mMap.addMarker(new MarkerOptions().position(madrid).title("Madrid-Atocha"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(madrid));
 
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onMapClick(@NonNull LatLng latLng) {
-        txtLatitud.setText(""+latLng.latitude);
-        txtLongitud.setText(""+latLng.longitude);
-
-
+        String calle = getAddressFromLatLng(latLng);
+        txtLatitudLongitud.setText("Lat: " + latLng.latitude + " Long: " + latLng.longitude);
+        txtCalle.setText("Calle: " + calle);
     }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public void onMapLongClick(@NonNull LatLng latLng) {
-        txtLatitud.setText(""+latLng.latitude);
-        txtLongitud.setText(""+latLng.longitude);
-
+        String calle = getAddressFromLatLng(latLng);
+        txtLatitudLongitud.setText("Lat: " + latLng.latitude + " Long: " + latLng.longitude);
+        txtCalle.setText("Calle: " + calle);
     }
+
+    private String getAddressFromLatLng(LatLng latLng) {
+        Geocoder geocoder = new Geocoder(Map_main.this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            if (addresses != null && addresses.size() > 0) {
+                Address address = addresses.get(0);
+                String streetAddress = address.getAddressLine(0); // Obtener la primera línea de la dirección
+                return streetAddress; // Devolver la dirección de la calle como una cadena
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
